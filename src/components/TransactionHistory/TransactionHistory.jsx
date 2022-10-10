52d389579c412e99d01acbade5610d8d78c7c38e
Nevrik-core <57769617+Nevrik-core@users.noreply.github.com>
2022-10-10 16:34:11 +0200

    fix mistake in proptypes

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -28,7 +28,7 @@ export const TransactionHistory = ({items}) => {
 };
 
 TransactionHistory.propTypes = {
-  transactions: PropTypes.arrayOf(
+  items: PropTypes.arrayOf(
     PropTypes.exact({
       id: PropTypes.string.isRequired,
       type: PropTypes.string.isRequired,
